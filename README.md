81ef4dc40ac4ce5e176a863f174a4dfb81853fce
sinsunran <115200753+sinsunran@users.noreply.github.com>
2022-11-17 10:56:19 +0900

    Set routers

---
+++ README.md
@@ -0,0 +1,16 @@
+#Youtube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search ->Search
+
+/edit-profile -> Edit Profile
+/users/edit
+/delte- profile ->Delete profile
+/users/delete
+
+/watch-video ->Watch Video
+
+/edit-video ->Edit Video
+/delete-video ->Delete Video
--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouters";
+import userRouter from "./routers/userRouters";
+import videoRouter from "./routers/videoRouters";
 
 const PORT = 5000;
 
 const app = express();
 
-app.get("/", (req, res) => {return res.end();});
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/",globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users",userRouter);
+
 
 app.listen(PORT, () => console.log(`Server listening on port http://localhost:${PORT}`));
